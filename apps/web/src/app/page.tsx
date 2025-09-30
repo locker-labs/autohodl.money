8324754fcafa0bb4a3cbdd091fa33120a70e297d
thisisashugupta <ashugpt1812@gmail.com>
2025-09-30 19:43:35 +0530

    feat: spend allowance to delegate

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,16 @@
+'use client';
+
+import { useAppKitAccount } from '@reown/appkit/react';
+import USDCApprovalChecker from '@/components/ApproveUSDC';
 import { ConnectButton } from '@/components/ConnectButton';
 
 export default function Home() {
+  const { isConnected } = useAppKitAccount();
   return (
     <div className={'min-h-screen flex flex-col items-center justify-center gap-4'}>
       <h1 className='text-4xl font-bold'>Welcome to AutoHodl</h1>
       <ConnectButton />
+      {isConnected ? <USDCApprovalChecker /> : null}
     </div>
   );
 }
