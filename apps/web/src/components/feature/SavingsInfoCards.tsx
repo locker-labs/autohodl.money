2926c559db3a8a9102a0ba25f2c6c5523752bb49
thisisashugupta <ashugpt1812@gmail.com>
2025-10-19 04:11:02 +0530

    feat: calculate Spare Change Saved

--- apps/web/src/components/feature/SavingsInfoCards.tsx
+++ apps/web/src/components/feature/SavingsInfoCards.tsx
@@ -1,14 +1,15 @@
 import { APYCard, RoundupSavingsCard, YieldCard } from '@/components/ui/cards';
-import { chain } from '@/config';
-import { useAutoHodl } from '@/context/AutoHodlContext';
+// import { chain } from '@/config';
+// import { useAutoHodl } from '@/context/AutoHodlContext';
 import { useAaveAPY } from '@/hooks/useAaveAPY';
 import { useAaveYieldBalance } from '@/hooks/useAaveYieldBalance';
-import { useTokenBalance } from '@/hooks/useTokenBalance';
-import { UsdcAddressMap } from '@/lib/constants';
-import type { Address } from 'viem';
+import { useSpareChange } from '@/hooks/useSpareChange';
+// import { useTokenBalance } from '@/hooks/useTokenBalance';
+// import { UsdcAddressMap } from '@/lib/constants';
+// import type { Address } from 'viem';
 
 export function SavingsInfoCards(): React.JSX.Element {
-  const { config } = useAutoHodl();
+  // const { config } = useAutoHodl();
 
   // Yield balance from Aave Yield
   const {
@@ -21,23 +22,25 @@ export function SavingsInfoCards(): React.JSX.Element {
   const { apy, loading: apyLoading } = useAaveAPY();
 
   // Savings address balance
-  const {
-    // balance: savingsBalanceBigInt,
-    isFetched: savingsBalanceFetched,
-    isFetching: savingsBalanceFetching,
-    balanceNumber: savingsBalance,
-    // balanceString: savingsBalanceString,
-  } = useTokenBalance({
-    token: UsdcAddressMap[chain.id],
-    address: config?.savingAddress as Address,
-  });
+  // const {
+  // balance: savingsBalanceBigInt,
+  // isFetched: savingsBalanceFetched,
+  // isFetching: savingsBalanceFetching,
+  // balanceNumber: savingsBalance,
+  // balanceString: savingsBalanceString,
+  // } = useTokenBalance({
+  //   token: UsdcAddressMap[chain.id],
+  //   address: config?.savingAddress as Address,
+  // });
+
+  const { changeSaved, isFetched: changeFetched, isFetching: changeFetching } = useSpareChange();
 
   return (
     <div className='grid grid-cols-1 sm:col-span-3 sm:grid-cols-3 gap-5'>
       {/* Current yield balance - spare change saved */}
       <YieldCard loading={!yieldBalanceFetched && yieldBalanceLoading} value={String(yieldBalance)} />
       <APYCard loading={apyLoading} value={apy} />
-      <RoundupSavingsCard loading={!savingsBalanceFetched && savingsBalanceFetching} value={String(savingsBalance)} />
+      <RoundupSavingsCard loading={!changeFetched && changeFetching} value={String(changeSaved)} />
     </div>
   );
 }
