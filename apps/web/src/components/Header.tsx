727789f30dff5f92c692cd196f1003afcb328c4f
thisisashugupta <ashugpt1812@gmail.com>
2025-12-05 03:11:08 +0530

    fix: css issues

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -5,10 +5,10 @@ import { ConnectButton } from '@/components/ConnectButton';
 
 export default function Header({ className }: { className?: string }) {
   return (
-    <div className={`w-full z-10 flex items-center justify-center bg-white border-b ${className}`}>
-      <div
-        className={' border-gray-300 sticky top-0 bg-white max-w-[1080px] p-4 w-full flex justify-between items-center'}
-      >
+    <div
+      className={`border-gray-300 w-full sticky top-0 z-10 flex items-center justify-center bg-white border-b ${className}`}
+    >
+      <div className={'max-w-[1080px] p-4 w-full flex justify-between items-center'}>
         <Image src='/AutoHODL.png' alt='AutoHodl logo' width={96} height={26} priority />
         <ConnectButton />
       </div>
