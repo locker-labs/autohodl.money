6aeec60d587c7be60cadfdb4518a957fa218ef08
thisisashugupta <ashugpt1812@gmail.com>
2026-01-18 16:00:29 +0530

    fix: github link, telegram icon

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import type { DocsThemeConfig } from 'nextra-theme-docs';
+import { TelegramSvg } from './components/TelegramSvg';
 
 const config: DocsThemeConfig = {
   logo: <span>autoHODL Docs</span>,
   primaryHue: { light: 110, dark: 110 }, // use autoHODL green for links
   primarySaturation: { light: 80, dark: 80 },
   project: {
-    link: 'https://github.com/locker/autohodl.money',
+    link: 'https://github.com/locker-labs/autohodl.money',
   },
   chat: {
     link: 'https://t.me/+713AuddkI9phNjUx',
+    icon: <TelegramSvg />,
   },
-  docsRepositoryBase: 'https://github.com/locker/autohodl.money/tree/main/apps/docs',
+  docsRepositoryBase: 'https://github.com/locker-labs/autohodl.money/tree/main/apps/docs',
   footer: {
     text: 'autoHODL Documentation',
   },
