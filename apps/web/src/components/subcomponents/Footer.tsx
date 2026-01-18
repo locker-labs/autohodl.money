a26c06b8ce80ac0d9e4672f5436331738e29761e
thisisashugupta <ashugpt1812@gmail.com>
2026-01-18 15:47:26 +0530

    feat: update footer links

--- apps/web/src/components/subcomponents/Footer.tsx
+++ apps/web/src/components/subcomponents/Footer.tsx
@@ -7,13 +7,14 @@ export function Footer({ innerClassName = '', className }: { innerClassName?: st
     { href: links.twitter, label: 'Twitter' },
     { href: links.github, label: 'GitHub' },
     { href: links.contact, label: 'Email' },
+    { href: links.docs, label: 'Documentation' },
   ];
 
   return (
     <div className={className}>
       <hr className='bg-[#D0D0D0] h-[2px] opacity-70' />
       <footer className={`px-[24px] w-full flex items-center justify-center py-[40px] ${innerClassName}`}>
-        <div className='max-w-[1080px] w-full flex items-start lg:items-center justify-between'>
+        <div className='max-w-[1080px] w-full flex flex-col sm:flex-row gap-4 sm:gap-0 items-center sm:items-start lg:items-center justify-between'>
           <button
             type='button'
             onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
@@ -21,7 +22,7 @@ export function Footer({ innerClassName = '', className }: { innerClassName?: st
           >
             auto<span className='font-bold'>HODL</span>
           </button>
-          <div className='flex flex-col sm:flex-row items-start lg:items-center justify-center gap-[12px] lg:gap-[32px] text-[16px] text-[#4D4A4A]'>
+          <div className='flex flex-col sm:flex-row items-center sm:items-start lg:items-center justify-center gap-[12px] lg:gap-[32px] text-[16px] text-[#4D4A4A]'>
             <p className='sm:hidden text-sm font-bold'>Connect</p>
             {footerLinks.map((item, idx) => (
               <ExternalLink href={item.href} key={`footer-link-${`${idx}`}`}>
