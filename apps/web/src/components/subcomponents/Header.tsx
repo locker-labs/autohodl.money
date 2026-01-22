17b7c8e967606e5ae524f0f9610fd0223c35391e
thisisashugupta <ashugpt1812@gmail.com>
2026-01-22 11:48:31 +0530

    remove: chain switcher from header

--- apps/web/src/components/subcomponents/Header.tsx
+++ apps/web/src/components/subcomponents/Header.tsx
@@ -2,7 +2,6 @@
 
 import Image from 'next/image';
 import { ConnectButton } from '@/components/feature/ConnectButton';
-import { ChainSelector } from '@/components/feature/ChainSelector';
 
 export default function Header({ className }: { className?: string }) {
   return (
@@ -12,7 +11,6 @@ export default function Header({ className }: { className?: string }) {
       <div className={'max-w-[1080px] p-4 w-full flex justify-between items-center gap-3'}>
         <Image src='/AutoHODL.png' alt='autoHODL' width={96} height={26} priority />
         <div className='flex items-center gap-3'>
-          <ChainSelector />
           <ConnectButton />
         </div>
       </div>
